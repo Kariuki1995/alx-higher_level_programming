0f52bcd96255826d020fd9c8e728592b71405241
Kenneth Kariuki <kenkariuki63@gmail.com>
2023-01-24 18:33:38 +0000

    0x06. Python - Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""An empty class Square that defines a square."""
 
 class Square:
-    """Represent a square."""
+    """class Square that defines a square."""
+
     pass
