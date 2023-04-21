70c25e391a4c53f68961d4d755a9eac718bd9d60
kenneth kariuki <kenkariuki63@gmail.com>
2023-04-21 19:37:30 +0000

    amended changes

--- 0-select_states.py
+++ 0-select_states.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Lists states"""
-
+"""Select from states table module."""
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    connect = MySQLdb.connect(
+                                host="localhost",
+                                port=3306,
+                                user=sys.argv[1],
+                                passwd=sys.argv[2],
+                                db=sys.argv[3],
+                                charset="utf8"
+                             )
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    response = cursor.fetchall()
+    for row in response:
         print(row)
-    cur.close()
-    conn.close()
+    cursor.close()
+    connect.close()
